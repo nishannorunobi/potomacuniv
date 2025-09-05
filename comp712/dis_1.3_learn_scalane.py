67c59eb603eadb07ffc2ac6b7a7f24c0901ad61d
Norun Nabi <norunnabinishan@gmail.com>
2025-09-05 18:58:46 -0400

    Update dis_1.3_learn_scalane.py

--- comp712/dis_1.3_learn_scalane.py
+++ comp712/dis_1.3_learn_scalane.py
@@ -28,8 +28,8 @@ def test_operations():
     immutable_time = time.perf_counter() - start_time
     immutable_time_ns = immutable_time * 10**9  # Convert to nanoseconds
 
-    print(f"Mutable Example Time: {mutable_time_ns:.4f} nano seconds")
-    print(f"Immutable Example Time: {immutable_time_ns:.4f} nano seconds")
+    print(f"Mutable Example Time: {mutable_time_ns:.2f} nano seconds")
+    print(f"Immutable Example Time: {immutable_time_ns:.2f} nano seconds")
     
 if __name__ == "__main__":
     test_operations()
